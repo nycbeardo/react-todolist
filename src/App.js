239428f42a66de0b3278a4c327cc624ff5db2ac8
Montique Stevens <mstevens528@gmail.com>
2019-06-20 22:50:54 -0400

    Update App.js
    added constructor for input and adding items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ToDoList from './ToDoList';
 
-function App() {
-  return (
-    <div className="App">
-     <ToDoList/>
-     <ToDoList addItem={this.addItem} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.sate = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+  handleInput = e => {
+    console.log('Hello Input')
+  }
+  addItem = () => {
+    console.log('Hello Add Item')
+  }
+  render() {
+    return (
+      <div className="App">
+        <ToDoList addItem={this.addItem} />
+      </div>
+    )
+  }
 }
 
 export default App;
